24639d0c3d71dc73f7a563235004b831e209e149
inna-dmytrenko <73888831+inna-dmytrenko@users.noreply.github.com>
2021-09-17 17:48:52 +0300

    test branch

--- contacts.js
+++ contacts.js
@@ -1,8 +1,26 @@
-const fs = require('fs')
+const fs = require('fs/promises')
 
-const fsRead = fs.readFile('db/contacts.json', 'utf-8', (error, data) => {
-  console.log(error)
-  console.log(data)
-})
+const readFile = async (filePath) => {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8')
+    console.table(data)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+readFile('db/contacts.json')
+
+const fileOperation = (async(filePath, (type = 'read'), (data = '')) = {})
+
+fileOperation('db/contacts.json').then((data) =>
+  console.log(data).catch((error) => console.log(error)),
+)
+
+// fs.readFile('db/contacts.json', 'utf-8')
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error))
+// console.log(error)
+// console.log(data)
+
 // console.log(fsRead)
-module.exports = fsRead
+// module.exports = fsRead
