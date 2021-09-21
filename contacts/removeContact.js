ebc3b78c8116fb7d2d90d6d98cc23d7f8f8b87bd
inna-dmytrenko <73888831+inna-dmytrenko@users.noreply.github.com>
2021-09-21 10:39:22 +0300

    fix-irst

--- contacts/removeContact.js
+++ contacts/removeContact.js
@@ -8,7 +8,6 @@ async function removeContact(id) {
     const contacts = await contactsOperations.getAll()
     const removeContact = await contactsOperations.getContactById(id)
     const newContacts = contacts.filter((contact) => String(contact.id) !== id)
-    //   contacts.filter((contact) => contact.id !== Number(id))
 
     if (!removeContact) {
       console.log(`Contact with such id=${id} cannot not found!`)
@@ -26,39 +25,4 @@ async function removeContact(id) {
   }
 }
 
-// async function updateContacts(newContacts) {
-//   try {
-//     // const contacts = await contactsOperations.getAll()
-//     // const contact = await contactsOperations.getAll()
-//     await fs.writeFile(contactsPath, JSON.stringify(newContacts))
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
-// // console.log(contacts)
-
-// async function removeContact(contactId) {
-//   try {
-//     const contacts = await contactsOperations.getAll()
-//     // const contact = await contactsOperations.getContactById(contactId)
-//     // console.log(contacts)
-//     const idx = contacts.findIndex((contact) => contact.id === contactId)
-//     console.log(idx)
-
-//     // console.log(cont)
-//     if (idx === -1) {
-//       return console.log(`Товар с id=${contactId} не найден`)
-//     }
-//     contacts.splice(idx, 1)
-//     // const newContacts = contacts.filter((item) => item.id !== contactId)
-//     // console.log(contacts)
-
-//     // const cont = contacts.splice(idx, 1)
-//     // console.log(cont)
-//     // await updateContacts(newContacts)
-//     // return contacts[idx]
-//   } catch (error) {
-//     console.log(error.message)
-//   }
-// }
 export default removeContact
